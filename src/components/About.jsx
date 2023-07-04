014022114180299d950687f762c8506aba6cce02
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-07-04 15:19:27 -0300

    refactoring

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,21 +4,23 @@ export default function About() {
   return (
     <div className={Style.body}>
       <h1> Juan Bosque</h1>
-      <h3>Full Stack Web Developer</h3>
+      <h3>Full Stack Web Developer
 
-      <h3>
+      <br/>
+      <br/>
+      <br/>
         Tech Skills: Javascript, React, Redux, CSS, Node, Express, Node,
         Sequelize, Postgres
       </h3>
-      <h4>
-        I am from Argentina, 37 years old, reliable, responsible. preferably in
-        The front, although I can also develop the rear. Am Passionate, loves to
-        learn new technologies or delve into concepts. I'm independent. I love
+      <p>
+        From Argentina, 37 years old, reliable, responsible. I prefer the
+        front, although I can also develop in back end too. I am Passionate, love
+        learning new technologies or delve into concepts. I am independent. I like
         solving problems and acquiring knowledge. I can do websites showing a
         home page, navigation bar, cards, details, inputs, searches,
         classifications, paging, routes, links, uploading files to cloudinary
         such as images, controlled component shapes, global and local states
-      </h4>
+      </p>
     </div>
   );
 }
