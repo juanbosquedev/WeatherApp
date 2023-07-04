095b86604d9f293de5c8436a0ac2ece85710cb32
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-07-04 15:35:24 -0300

    factoring

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -29,7 +29,7 @@ export default function Card({
           Style="text-decoration:none"
           to={`/details/:${id}`}
         >
-          +
+          Info
         </Link>
       </div>
     </div>
